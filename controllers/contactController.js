469018b5d6e31fbc72624f2d0281588496a729cf
makedonec88 <makedonec88@gmail.com>
2017-12-18 22:53:40 +0100

    Added: form validation status

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -8,23 +8,23 @@ const bot = require('../bot/bot')
 
 exports.showFormErrors = (req, res, next) => {
   const err = validationResult(req);
-  console.log('form body', req.body)
+  console.log('form body' + new Date().getMinutes(), req.body)
 
   if (!err.isEmpty()) {
     console.log(err.mapped())
     return
-    // TODO: throw server error here
+    res.json({valid: false})
   }
 
   const {name, email, phone, message} = req.body
 
-  bot.sendGroupMessage(
-    '*name* = ' + name + ',\n' +
-    '*email* = ' + email + ',\n' +
-    '*phone* = ' + phone + ',\n' +
-    '*message* = ' + message
-  )
+  // bot.sendGroupMessage(
+  //   '*name* = ' + name + ',\n' +
+  //   '*email* = ' + email + ',\n' +
+  //   '*phone* = ' + phone + ',\n' +
+  //   '*message* = ' + message
+  // )
 
-  res.send('OK');
+  res.json({valid: true})
 
 };
